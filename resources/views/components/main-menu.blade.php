c9fa3f3c80d8751081909dbeb2243f0b4950d78b
Hatim EL OUFIR <heloufir@alvariumgroup.com>
2022-09-09 23:00:14 +0100

    Design enhancement

--- resources/views/components/main-menu.blade.php
+++ resources/views/components/main-menu.blade.php
@@ -1,4 +1,10 @@
-<aside class="main-menu h-full" aria-label="Sidebar">
+<aside class="main-menu h-full relative" aria-label="Sidebar" :class="{ 'closed': !sidebar_opened }" x-data="{ sidebar_opened: (window.innerWidth < 640 ? false : true) }">
+    <button x-on:click="sidebar_opened = !sidebar_opened" type="button" class="absolute top-7 -right-11 w-12 h-12 rounded-tr-full rounded-br-full shadow hover:shadow-lg bg-gray-50 text-gray-500 hover:bg-primary-500 hover:text-white text-xl">
+        <i class="fa" :class="{
+            'fa-angle-left': sidebar_opened,
+            'fa-angle-right': !sidebar_opened
+        }"></i>
+    </button>
     <div class="overflow-y-auto py-10 px-5 bg-gray-50 flex flex-col justify-start items-start rounded dark:bg-gray-800 h-full border-r border-gray-50 drop-shadow-xl">
         <a href="{{ route('home') }}" class="flex items-center pl-2.5 mb-5">
             <img src="{{ asset('images/logo.png') }}" class="mr-3 h-6 sm:h-7" alt="{{ config('app.name') }}" />
