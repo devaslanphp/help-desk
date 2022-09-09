d942618ae6d0848fdcc7334e55a175fb9bcfa5d0
Hatim EL OUFIR <heloufir@alvariumgroup.com>
2022-09-09 23:04:51 +0100

    Design enhancement

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
     <div class="absolute w-full h-full top-0 left-0 right-0 bottom-0 overflow-auto flex flex-row justify-start items-start gap-0">
         <x-main-menu />
 
-        <div class="w-full bg-gray-50 h-full">
+        <div class="w-full bg-white h-full">
             {{$slot}}
         </div>
     </div>
