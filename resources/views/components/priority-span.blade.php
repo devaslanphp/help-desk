d37dbe29dd1919eeda641a3d7aa838231523b503
Hatim EL OUFIR <heloufir@alvariumgroup.com>
2022-09-11 20:22:50 +0100

    Tickets design enhancement

--- resources/views/components/priority-span.blade.php
+++ resources/views/components/priority-span.blade.php
@@ -1,5 +1,6 @@
 @if($priority && config('system.priorities.' . $priority))
-    <span class="px-3 py-1 rounded-full text-sm {{ __(config('system.priorities.' . $priority . '.bg-color')) }} {{ __(config('system.priorities.' . $priority . '.text-color')) }}">
+    <span class="px-3 py-1 rounded-full flex flex-row justify-center items-center gap-2 text-sm {{ __(config('system.priorities.' . $priority . '.bg-color')) }} {{ __(config('system.priorities.' . $priority . '.text-color')) }}">
+        <i class="fa {{ config('system.priorities.' . $priority . '.icon') }}"></i>
         {{ __(config('system.priorities.' . $priority . '.title')) }}
     </span>
 @endif
