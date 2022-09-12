f071da788a35db8eb4f90b57fb6fbee4c6de8ddb
Hatim EL OUFIR <heloufir@alvariumgroup.com>
2022-09-12 14:53:02 +0100

    Design enhancement

--- resources/views/livewire/analytics.blade.php
+++ resources/views/livewire/analytics.blade.php
@@ -67,50 +67,39 @@
                 </div>
             </div>
             <div class="w-full p-5">
-                <div class="w-full flex flex-col gap-5 p-5 rounded-lg border border-gray-100 shadow-lg bg-white">
-                    <span class="text-lg text-gray-500 font-medium">@lang('Not assigned tickets')</span>
+                <div class="w-full flex flex-col justify-center items-center gap-5 p-5 rounded-lg border border-gray-100 shadow-lg bg-white">
+                    <span class="text-lg text-gray-500 font-medium w-full text-left">@lang('Tickets by statuses')</span>
+                    <div class="overflow-x-auto relative sm:rounded-lg w-full">
+                        <canvas id="ticketsByStatuses" style="height: 200px;"></canvas>
+                    </div>
                     <div class="w-full overflow-x-auto relative sm:rounded-lg">
                         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="py-3 px-6">
-                                    @lang('Type')
-                                </th>
-                                <th scope="col" class="py-3 px-6">
-                                    @lang('Priority')
-                                </th>
-                                <th scope="col" class="py-3 px-6">
-                                    @lang('Title')
-                                </th>
-                                <th scope="col" class="py-3 px-6">
                                     @lang('Status')
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    @lang('Tickets')
                                 </th>
                             </tr>
                             </thead>
                             <tbody>
-                            @if($notAssignedTickets->count())
-                                @foreach($notAssignedTickets as $ticket)
+                            @if(sizeof($ticketsByStatuses))
+                                @foreach($ticketsByStatuses as $status => $count)
                                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                         <td class="py-4 px-6">
-                                            <x-type-span :type="$ticket->type" :min="true" />
+                                            {{ $status }}
                                         </td>
                                         <td class="py-4 px-6">
-                                            <x-priority-span :priority="$ticket->priority" />
-                                        </td>
-                                        <td class="py-4 px-6">
-                                            <a href="{{ route('tickets.details', ['ticket' => $ticket, 'slug' => Str::slug($ticket->title)]) }}" class="text-gray-500 text-sm hover:underline hover:text-primary-500">
-                                                {{ $ticket->title }}
-                                            </a>
-                                        </td>
-                                        <td class="py-4 px-6">
-                                            <x-status-span :status="$ticket->status" />
+                                            {{ $count }}
                                         </td>
                                     </tr>
                                 @endforeach
                             @else
                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                     <td colspan="4" class="py-4 px-6 text-center dark:text-white">
-                                        @lang('All tickets are assigned!')
+                                        @lang('No tickets configured!')
                                     </td>
                                 </tr>
                             @endif
@@ -164,39 +153,50 @@
                 </div>
             </div>
             <div class="w-full p-5">
-                <div class="w-full flex flex-col justify-center items-center gap-5 p-5 rounded-lg border border-gray-100 shadow-lg bg-white">
-                    <span class="text-lg text-gray-500 font-medium w-full text-left">@lang('Tickets by statuses')</span>
-                    <div class="overflow-x-auto relative sm:rounded-lg w-full">
-                        <canvas id="ticketsByStatuses" style="height: 200px;"></canvas>
-                    </div>
+                <div class="w-full flex flex-col gap-5 p-5 rounded-lg border border-gray-100 shadow-lg bg-white">
+                    <span class="text-lg text-gray-500 font-medium">@lang('Not assigned tickets')</span>
                     <div class="w-full overflow-x-auto relative sm:rounded-lg">
                         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="py-3 px-6">
-                                    @lang('Status')
+                                    @lang('Type')
                                 </th>
                                 <th scope="col" class="py-3 px-6">
-                                    @lang('Tickets')
+                                    @lang('Priority')
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    @lang('Title')
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    @lang('Status')
                                 </th>
                             </tr>
                             </thead>
                             <tbody>
-                            @if(sizeof($ticketsByStatuses))
-                                @foreach($ticketsByStatuses as $status => $count)
+                            @if($notAssignedTickets->count())
+                                @foreach($notAssignedTickets as $ticket)
                                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                         <td class="py-4 px-6">
-                                            {{ $status }}
+                                            <x-type-span :type="$ticket->type" :min="true" />
                                         </td>
                                         <td class="py-4 px-6">
-                                            {{ $count }}
+                                            <x-priority-span :priority="$ticket->priority" />
+                                        </td>
+                                        <td class="py-4 px-6">
+                                            <a href="{{ route('tickets.details', ['ticket' => $ticket, 'slug' => Str::slug($ticket->title)]) }}" class="text-gray-500 text-sm hover:underline hover:text-primary-500">
+                                                {{ $ticket->title }}
+                                            </a>
+                                        </td>
+                                        <td class="py-4 px-6">
+                                            <x-status-span :status="$ticket->status" />
                                         </td>
                                     </tr>
                                 @endforeach
                             @else
                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                     <td colspan="4" class="py-4 px-6 text-center dark:text-white">
-                                        @lang('No tickets configured!')
+                                        @lang('All tickets are assigned!')
                                     </td>
                                 </tr>
                             @endif
