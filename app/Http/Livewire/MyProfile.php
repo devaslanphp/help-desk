83eb884e52c6423ae40293705972c11fd19c278f
Hatim EL OUFIR <heloufir@alvariumgroup.com>
2022-09-12 14:00:47 +0100

    My profile update function

--- app/Http/Livewire/MyProfile.php
+++ app/Http/Livewire/MyProfile.php
@@ -2,12 +2,109 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Notifications\Notification;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 use Livewire\Component;
+use Phpsa\FilamentPasswordReveal\Password;
+use Closure;
 
-class MyProfile extends Component
+class MyProfile extends Component implements HasForms
 {
+    use InteractsWithForms;
+
+    public User|Authenticatable|null $user;
+
+    public function mount(): void
+    {
+        $this->initProfile();
+    }
+
     public function render()
     {
         return view('livewire.my-profile');
+    }
+
+    /**
+     * Initialize form data
+     *
+     * @return void
+     */
+    private function initProfile(): void
+    {
+        $this->user = User::where('id', auth()->user()->id)->first();
+        $this->form->fill([
+            'name' => $this->user->name,
+            'email' => $this->user->email
+        ]);
+        if (session()->has('profile_updated')) {
+            Notification::make()
+                ->success()
+                ->title('Profile updated')
+                ->body(__('Your account details has been updated'))
+                ->send();
+        }
+    }
+
+    /**
+     * Form schema definiation
+     *
+     * @return array
+     */
+    protected function getFormSchema(): array
+    {
+        return [
+            TextInput::make('name')
+                ->label(__('Full name'))
+                ->maxLength(255)
+                ->required(),
+
+            TextInput::make('email')
+                ->label(__('Email address'))
+                ->email()
+                ->unique(table: User::class, column: 'email', ignorable: fn() => $this->user->id ? $this->user : null)
+                ->required(),
+
+            Password::make('current_password')
+                ->label(__('Current password'))
+                ->required(),
+
+            Grid::make()
+                ->schema([
+                    Password::make('new_password')
+                        ->label(__('New password'))
+                        ->rule('confirmed'),
+
+                    Password::make('new_password_confirmation')
+                        ->label(__('Password confirmation'))
+                        ->dehydrated(false),
+                ]),
+        ];
+    }
+
+    public function save(): void
+    {
+        $data = $this->form->getState();
+        if (Hash::check($data['current_password'], $this->user->password)) {
+            $this->user->name = $data['name'];
+            $this->user->email = $data['email'];
+            if ($data['new_password']) {
+                $this->user->password = bcrypt($data['new_password']);
+            }
+            $this->user->save();
+            session()->flash('profile_updated', true);
+            redirect()->to(route('my-profile'));
+        } else {
+            throw ValidationException::withMessages([
+                'current_password' => __('The password entered is incorrect.')
+            ]);
+        }
     }
 }
--- resources/views/livewire/my-profile.blade.php
+++ resources/views/livewire/my-profile.blade.php
@@ -11,4 +11,33 @@
             </span>
         </div>
     </div>
+
+    <form wire:submit.prevent="save" class="xl:w-2/5 lg:w-3/5 md:w-4/5 w-full">
+        <div class="w-full flex flex-row justify-start items-center gap-3 p-5">
+            <x-user-avatar :user="$user" :size="100" />
+            <div class="flex flex-col justify-center items-start gap-0">
+                <span class="text-lg text-gray-500 font-medium">{{ $user->name }}</span>
+                <span class="text-base text-gray-500 font-light">{{ $user->email }}</span>
+                <span class="text-sm text-gray-400 font-light">
+                    @lang('Account created:') {{ $user->created_at->diffForHumans() }}
+                </span>
+                <span class="text-sm text-gray-400 font-light">
+                    @lang('Last update:') {{ $user->updated_at->diffForHumans() }}
+                </span>
+            </div>
+        </div>
+
+        <div class="w-full p-5">
+            {{ $this->form }}
+        </div>
+
+        <div class="w-full flex flex-row gap-2 justify-between items-center border-t border-gray-200 mt-5 pb-5 px-5">
+            <button type="submit" wire:loading.attr="disabled" class="rounded-lg flex flex-row justify-center items-center text-center gap-2 text-white bg-primary-700 bg-opacity-90 hover:bg-opacity-100 shadow hover:shadow-lg px-10 py-3 text-sm mt-5">
+                @lang('Save')
+                <div wire:loading>
+                    <i class="fa fa-spin fa-spinner"></i>
+                </div>
+            </button>
+        </div>
+    </form>
 </div>
